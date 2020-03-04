1ba2573996b32feab9bd6afe3fd5256ac6f0f91e
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-03 23:42:59 -0500

    Add files via upload

---
+++ FormatStringsOfNames/Names.js
@@ -0,0 +1,33 @@
+// Instructions
+// Given: an array containing hashes of names
+
+// Return: a string formatted as a list of names separated by
+//  commas except for the last two names,
+//   which should be separated by an ampersand.
+
+//Link
+// https: www.codewars.com/kata/53368a47e38700bd8300030d/train/javascript
+
+//Solution
+
+function list(names) {
+  let result = "";
+
+  if (names.length === 1) {
+    return names[0].name;
+  } else if (names.length === 2) {
+    return names[0].name + " & " + names[1].name;
+  } else if (names.length === 0) {
+    return "";
+  } else {
+    for (var i = 0; i < names.length - 2; i++) {
+      result += names[i].name + ", ";
+    }
+    result =
+      result +
+      names[names.length - 2].name +
+      " & " +
+      names[names.length - 1].name;
+    return result;
+  }
+}
