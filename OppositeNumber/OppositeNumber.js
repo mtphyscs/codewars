619aa54499f7ad56deae273787c3472039ddd2f6
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-15 15:26:42 -0400

    done

---
+++ OppositeNumber/OppositeNumber.js
@@ -0,0 +1,10 @@
+// INSTRUCTIONS
+// Very simple, given a number, find its opposite.
+
+// LINK
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+// SOLUTION
+function opposite(number) {
+    return -number;
+  }
