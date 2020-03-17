4196653a6965b2b55d8e785ef0e812429dd89587
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-17 11:23:02 -0400

    done

---
+++ ReturnNegative/returnNegative.js
@@ -0,0 +1,15 @@
+// INSTRUCTIONS
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+
+// LINK
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+
+
+// SOLUTIONS
+
+
+function makeNegative(num) {
+    return num < 0 ? num : -num;
+   }
