0e9513f81156110c38f513fda527ed6a0bb4d8de
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-18 18:21:47 -0400

    done

---
+++ MakeUpperCase/MakeUpperCase.js
@@ -0,0 +1,13 @@
+// INSTRUCTIONS 
+// Write function makeUpperCase.
+
+
+
+// LINK
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+
+
+
+// SOLUTION
+
+const makeUpperCase = str => str.toUpperCase();
