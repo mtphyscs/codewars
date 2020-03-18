1fb562c1a28793c3d4af8851e6e511250a700182
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-17 22:17:13 -0400

    done

---
+++ SumOfPositive/SumOfPositive.js
@@ -0,0 +1,22 @@
+// INSTRUCTIONS
+
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+
+
+// LINK
+
+// https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+
+
+
+// SOLUTION
+
+
+function positiveSum(arr) {
+    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+ }
