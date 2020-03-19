fe0fcf0af4c5693e1da0eea0f9a4ec690047eb3e
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-19 12:59:13 -0400

    done

---
+++ RockPapperScissors/RockPaperScissors.js
@@ -0,0 +1,30 @@
+// INSTRUCTIONS
+
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples:
+
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
+
+// LINK
+
+// https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/javascript
+
+
+// SOLUTION
+
+const rps = (p1, p2) => {
+    if (p1 == p2)
+      return 'Draw!';
+      
+     if (p1 == 'rock' && p2 == 'scissors') 
+       return 'Player 1 won!'
+     else if (p1 == 'scissors' && p2 == 'paper') 
+       return 'Player 1 won!'
+     else if (p1 == 'paper' && p2 == 'rock') 
+       return 'Player 1 won!'
+     else
+       return 'Player 2 won!';
+  };
