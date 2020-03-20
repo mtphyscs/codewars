8d4acbcb66b805a5ec27c7fce1ba729bd57f30e0
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-20 16:11:06 -0600

    done

---
+++ RemoveStringSpaces/RemoveStringSpaces.js
@@ -0,0 +1,17 @@
+// INSTRUCTIONS
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+// LINK
+
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+
+
+
+// SOLUTIONS
+
+function noSpace(x){
+    return x.replace(/\s/g, "");
+  }
+
