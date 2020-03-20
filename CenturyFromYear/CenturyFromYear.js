fc6114b05a9d38fd99461bc1859e0cdb072e58f1
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-20 16:25:06 -0600

    done

---
+++ CenturyFromYear/CenturyFromYear.js
@@ -0,0 +1,27 @@
+// INSTRUCTIONS
+
+// The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+// Task :
+// Given a year, return the century it is in.
+
+// Input , Output Examples ::
+// centuryFromYear(1705)  returns (18)
+// centuryFromYear(1900)  returns (19)
+// centuryFromYear(1601)  returns (17)
+// centuryFromYear(2000)  returns (20)
+
+
+
+// LINK
+
+// https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
+
+
+
+// SOLUTION
+
+function century(year) {
+    return (year + 99) / 100 | 0;
+  }
+
