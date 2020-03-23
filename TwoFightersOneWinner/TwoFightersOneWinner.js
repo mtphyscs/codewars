e121a85f0dd6c429dc5e16b4444769d3a1bce93b
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-22 19:05:29 -0600

    done

---
+++ TwoFightersOneWinner/TwoFightersOneWinner.js
@@ -0,0 +1,30 @@
+// INSTRUCTIONS
+
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// LINK
+
+// https://www.codewars.com/kata/577bd8d4ae2807c64b00045b/train/javascript
+
+// SOLUTION
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  while (fighter1.health > 0 && fighter2.health > 0) {
+    fighter2.health -= fighter1.damagePerAttack;
+    fighter1.health -= fighter2.damagePerAttack;
+  }
+
+  if (fighter1.health <= 0 && fighter2.health <= 0) {
+    return firstAttacker;
+  } else if (fighter1.health <= 0) {
+    return fighter2.name;
+  } else {
+    return fighter1.name;
+  }
+}
