9088f2b9e80b20eeffcc5f70980b493675968ce5
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-22 20:57:51 -0600

    done

---
+++ StringRepeat/StringRepeat.js
@@ -0,0 +1,16 @@
+// INSTRUCTIONS 
+
+// Write a function called repeatString which repeats the given String src exactly count times.
+
+
+// LINK
+
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+
+
+
+// SOLUTION
+
+function repeatStr (n, s) {
+    return  s.repeat(n);
+  }
