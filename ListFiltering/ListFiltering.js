1551072ab0c5a822ea96b4dd0726206e354af79c
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-27 12:14:07 -0600

    done

---
+++ ListFiltering/ListFiltering.js
@@ -0,0 +1,20 @@
+// INSTRUCTIONS
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+
+
+// LINK
+
+// https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript
+
+
+
+// SOLUTION
+
+
+function filter_list(l) {
+  return l.filter(Number.isInteger);
+}
+
+
