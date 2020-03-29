fc2ece47bc5b2fa00b0c1b0ea309297703d2d55a
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-28 18:09:54 -0600

    done

---
+++ LostWithoutAMap/LostWithoutAMap.js
@@ -0,0 +1,25 @@
+// INSTRUCTIONS
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+
+// LINK
+
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/solutions
+
+
+
+// SOLUTION
+
+
+function maps(x) {
+  return x.map(n => n * 2);
+}
+
+
