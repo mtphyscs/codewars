cac2a3a0476c0f38711f3b3e2e5e7d0444dcd7fb
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-03-29 22:28:39 -0600

    done

---
+++ EvenOrOdd/EvenOrOdd.js
@@ -0,0 +1,25 @@
+// INSTRUCTIONS
+
+// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+
+
+// LINK
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+
+
+
+// SOLUTION
+
+function even_or_odd(number) {
+  if (number % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+
