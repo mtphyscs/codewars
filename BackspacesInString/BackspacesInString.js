5b02ea11ca4f1dcfcee1637b0af1834bb3f6788c
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-04-01 10:06:24 -0600

    done

---
+++ BackspacesInString/BackspacesInString.js
@@ -0,0 +1,18 @@
+// INSTRUCTIONS
+
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+// Your task is to process a string with "#" symbols.
+
+
+
+// LINK
+
+// https://www.codewars.com/kata/5727bb0fe81185ae62000ae3/train/javascript
+
+
+
+// SOLUTION
+
+cleanString = s => s.split('').reduce((r, e) => e == '#' ? r.slice(0, -1) : r + e, '');
+
