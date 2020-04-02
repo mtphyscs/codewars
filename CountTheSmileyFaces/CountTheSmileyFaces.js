0aa2d7669af8ad846434954e6c3c39115a235f0c
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-04-02 16:01:15 -0600

    done

---
+++ CountTheSmileyFaces/CountTheSmileyFaces.js
@@ -0,0 +1,26 @@
+// INSTRUCTIONS
+
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+// Rules for a smiling face:
+// -Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// -A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// -Every smiling face must have a smiling mouth that should be marked with either ) or D.
+// No additional characters are allowed except for those mentioned.
+// Valid smiley face examples:
+// :) :D ;-D :~)
+// Invalid smiley faces:
+// ;( :> :} :]
+
+
+
+// LINK
+
+// https://www.codewars.com/kata/583203e6eb35d7980400002a/train/javascript
+
+
+
+// SOLUTION
+
+let countSmileys = arr => arr.filter(a => /[:|;][\-|\~]?[)|D]/.test(a)).length;
+
+
