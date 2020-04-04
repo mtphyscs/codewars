d7056a9279323eda5997a6818b016634b93f2eb6
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-04-04 14:43:11 -0600

    done

---
+++ HighestScoringWord/HighestScoringWord.js
@@ -0,0 +1,37 @@
+// INSTRUCTIONS
+
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+
+
+// LINK
+
+// https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/javascript
+
+
+
+// SOLUTION
+
+function high(x) {
+  x = x.split(" ");
+  let [max, word] = [0, ""];
+  for (let i = 0; i < x.length; i++) {
+    let temp = x[i].length
+      ? x[i]
+          .split("")
+          .map((a) => a.charCodeAt(0) - 96)
+          .reduce((a, b) => a + b)
+      : x[i].charCodeAt(0);
+    if (temp > max) [word, max] = [x[i], temp];
+  }
+  return word;
+}
+
