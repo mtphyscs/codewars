27b870ed796eb42429f3be84149db6b21e1c2b85
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-04-07 20:09:36 -0600

    done

---
+++ ConsecutiveStrings/ConsecutiveStrings.js
@@ -0,0 +1,33 @@
+// INSTRUCTIONS
+
+// You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+// Example:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+
+// Note
+// consecutive strings : follow one after another without an interruption
+
+
+// LINK
+
+// https://www.codewars.com/kata/56a5d994ac971f1ac500003e/train/javascript
+
+
+// SOLUTION
+
+function longestConsec(strarr, k) {
+  if (strarr === [] || k > strarr.length || k <= 0) {
+    return "";
+  }
+  let str = "";
+  for (let i = 0; i < strarr.length; i++) {
+    let temp = strarr.slice(i, i + k).join("");
+    if (temp.length > str.length) {
+      str = temp;
+    }
+  }
+  return str;
+}
