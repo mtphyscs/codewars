714f5c39157906ebbd940ebe55e081c53d93d4a5
形而上学 <47684055+mtphyscs@users.noreply.github.com>
2020-04-21 12:52:47 -0600

    done

---
+++ PrefillAnArray/PrefillAnArray.js
@@ -0,0 +1,25 @@
+// INSTRUCTIONS
+
+// Create the function prefill that returns an array of n elements that all have the same value v. See if you can do this without using a loop.
+
+// You have to validate input:
+
+// v can be anything (primitive or otherwise)
+// if v is ommited, fill the array with undefined
+// if n is 0, return an empty array
+// if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError
+// When throwing a TypeError, the message should be n is invalid, where you replace n for the actual value passed to the function.
+
+
+// LINK
+
+// https://www.codewars.com/kata/54129112fb7c188740000162/train/javascript
+
+
+// SOLUTION
+
+function prefill(n, v) {
+  if (!Number.isInteger(+n) || +n < 0 || typeof n === "boolean")
+    throw new TypeError(`${n} is invalid`);
+  return Array.from(Array(+n), (x) => v);
+}
