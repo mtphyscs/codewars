b0001634d8b15f998c69d864ade174d96bac6b5e
形而上学 <47684055+mtphyscs@users.noreply.github.com>
2020-06-14 22:36:54 -0600

    done

---
+++ multiplesof3or5/multiplesof3or5.js
@@ -0,0 +1,23 @@
+// INSTRUCTIONS
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+
+// LINK
+
+// https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+
+
+// SOLUTION
+
+function solution(number) {
+  var sum = 0;
+  for (var i = 3; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
