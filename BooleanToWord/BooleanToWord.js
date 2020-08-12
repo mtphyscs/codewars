04c528e5ef19311827de957e85373a68b39b0fa8
形而上学 <47684055+mtphyscs@users.noreply.github.com>
2020-08-11 23:17:29 -0600

    done

---
+++ BooleanToWord/BooleanToWord.js
@@ -0,0 +1,17 @@
+// INSTRUCTIONS
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// LINK
+
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+// SOLUTION
+
+function boolToWord(bool) {
+    if (bool) {
+        return 'Yes';
+    } else {
+        return 'No';
+    }
+}
