914f64b7a87e8a28a1b8a4d1243b1ee4236b6859
形而上学 <47684055+mtphyscs@users.noreply.github.com>
2020-08-21 16:04:29 -0600

    done

---
+++ KataBroken/KataBroken.js
@@ -0,0 +1,19 @@
+// INSTRUCTIONS
+
+// (Imagine the following spoken in the voice of GlaDOS from Portal...)
+
+// We regret to inform you that this next kata is impossible. Make no attempt to solve it.
+
+// Do not be fooled by anyone who tells you to "think outside the box" or "know when to break the rules." They obviously just want to encourage you to try so they can watch you fail.
+
+// Again, we apologize for this clearly broken kata.
+
+
+// LINK 
+
+// https://www.codewars.com/kata/58794dffb2ed48d83e00185b/train/javascript
+
+
+// SOLUTION
+
+const solution = ( i => () => "GlaDOS".charCodeAt(i++%6) ) ( 0 ) ;
